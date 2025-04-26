a8e506b23e2116ffd4837a7a08cbbd1bc16375c5
mohyware <mohieelden50@gmail.com>
2025-04-26 17:41:40 +0300

    init for working on render

--- app/components/editor/AssetsPanel/SidebarButtons/ExportButton.tsx
+++ app/components/editor/AssetsPanel/SidebarButtons/ExportButton.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 
 export default function ExportButton({ onClick }: { onClick: () => void }) {
+
     return (
         <button
             className="bg-white border border-solid border-transparent transition-colors flex flex-col items-center justify-center text-gray-800 hover:bg-[#ccc] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-auto py-2 px-2 sm:px-5 sm:w-auto"
---
+++ app/components/editor/AssetsPanel/tools-section/ExportList.tsx
@@ -0,0 +1,8 @@
+import Export from "../../render/Ffmpeg/Export";
+export default function ExportList() {
+    return (
+        <div className="flex flex-col gap-4">
+            <Export />
+        </div>
+    )
+}
