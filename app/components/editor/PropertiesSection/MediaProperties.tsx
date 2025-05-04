8e86b6f8925476e8cbc86fd978321ff80f001a88
mohyware <mohieelden50@gmail.com>
2025-05-04 16:43:48 +0300

    remove volume from media probs for image

--- app/components/editor/PropertiesSection/MediaProperties.tsx
+++ app/components/editor/PropertiesSection/MediaProperties.tsx
@@ -97,7 +97,7 @@ export default function MediaProperties() {
                     </div>
                 </div>
                 {/* Visual Properties */}
-                <div className="space-y-2">
+                <div className="space-y-6">
                     <h4 className="font-semibold">Visual Properties</h4>
                     <div className="grid grid-cols-2 gap-4">
                         <div>
@@ -161,9 +161,18 @@ export default function MediaProperties() {
                             />
                         </div>
                     </div>
+                    <button
+                        onClick={() => onDeleteMedia(mediaFile.id)}
+                        className="text-red-500 hover:text-red-700 flex items-center gap-2"
+                    >
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-7 w-7" viewBox="0 0 20 20" fill="currentColor">
+                            <path fillRule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clipRule="evenodd" />
+                        </svg>
+                        <span>Remove Media</span>
+                    </button>
                 </div>
                 {/* Audio Properties */}
-                <div className="space-y-2">
+                {(mediaFile.type === "video" || mediaFile.type === "audio") && <div className="space-y-2">
                     <h4 className="font-semibold">Audio Properties</h4>
                     <div className="grid grid-cols-1 gap-4">
                         <div>
@@ -192,17 +201,8 @@ export default function MediaProperties() {
                             />
                         </div> */}
                     </div>
-                </div>
+                </div>}
                 <div >
-                    <button
-                        onClick={() => onDeleteMedia(mediaFile.id)}
-                        className="text-red-500 hover:text-red-700 flex items-center gap-2"
-                    >
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-7 w-7" viewBox="0 0 20 20" fill="currentColor">
-                            <path fillRule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clipRule="evenodd" />
-                        </svg>
-                        <span>Remove Media</span>
-                    </button>
                 </div>
             </div>
         </div >
